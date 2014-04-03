e3f766d53344221fb13e6e14a278dfc2dabfb542
monk-ee <magic.monkee.magic@gmail.com>
2014-04-04 09:42:20 +1000

    added basic assertion tests - refactored init into methods

--- test_theSleeper.py
+++ test_theSleeper.py
@@ -7,25 +7,33 @@ __project__ = 'TheSleeper'
 
 class TestTheSleeper(TestCase):
     def test_stop_instance(self):
-        self.fail()
+        ts = TheSleeper
+        self.assertRaises(Exception,ts.stop_instance)
 
     def test_start_instance(self):
-        self.fail()
+        ts = TheSleeper
+        self.assertRaises(Exception,ts.start_instance)
 
     def test_search_for_tagged(self):
-        self.fail()
+        ts = TheSleeper
+        self.assertRaises(Exception,ts.search_for_tagged)
 
     def test_search_for_untagged_to_stop(self):
-        self.fail()
+        ts = TheSleeper
+        self.assertRaises(Exception,ts.search_for_untagged_to_stop)
 
     def test_parse_sleeper_tags(self):
-        self.fail()
+        ts = TheSleeper
+        self.assertRaises(Exception,ts.parse_sleeper_tags)
 
     def test_cron_stop(self):
-        self.fail()
+        ts = TheSleeper
+        self.assertRaises(Exception,ts.cron_stop)
 
     def test_cron_start(self):
-        self.fail()
+        ts = TheSleeper
+        self.assertRaises(Exception,ts.cron_start)
 
     def test_sns_message(self):
-        self.fail()
+        ts = TheSleeper
+        self.assertRaises(Exception,ts.sns_message)
